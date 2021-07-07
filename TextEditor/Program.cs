c5c3059c21036d40393ede41b985818ad803ff1e
Thiago Rodrigues <thiagothogs@gmail.com>
2021-07-07 02:10:09 -0300

    created application menu

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -4,9 +4,37 @@ namespace TextEditor
 {
     static class Program
     {
-        private static void Main()
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que vc deseja fazer?");
+            Console.WriteLine("1 - Abrir um arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine() ?? string.Empty);
+
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        private static void Abrir()
+        {
+            
+        }
+
+        private static void Editar()
+        {
+            
         }
     }
 }
