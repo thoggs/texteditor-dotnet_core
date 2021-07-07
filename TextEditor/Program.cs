4b598a6dd6c2b63439ad84c05f0c1d9c980a58ca
Thiago Rodrigues <thiagothogs@gmail.com>
2021-07-07 02:38:37 -0300

    add program structure

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -34,7 +34,18 @@ namespace TextEditor
 
         private static void Editar()
         {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("---------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
             
+            Console.Write(text);
         }
     }
 }
